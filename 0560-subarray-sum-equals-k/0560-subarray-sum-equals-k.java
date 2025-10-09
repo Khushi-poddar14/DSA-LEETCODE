0dcb95b78a0cb47b0ea113b7f771ae1982bac956
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-10-10 00:44:31 +0530

    Time: 1091 ms (15.51%), Space: 46.6 MB (43.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,10 +1,10 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
         int count = 0;
+        int n = nums.length;
         for(int i = 0; i < n; i++){
             int sum = 0;
-            for(int j = i; j < n; j++){       // agr ham j = i + 1 likhte h toh single element subarray skip hojata h
+            for(int j = i; j < n; j++){
                 sum += nums[j];
                 if(sum == k){
                     count++;
@@ -14,3 +14,7 @@ class Solution {
         return count;
     }
 }
+
+// basically j = i isliye likha h becz the possible subarrays for [1,2,3] are [1], [2], [3], [1,2], [2,3] , 
+//we have written sum += nums[j] becz ham ek baar nums[i] ki value toh sum m store krva hi rhe hai toh ab jaise jaise j aage bdhega sirf uski value add krne ki zrurat hai hme , 
+//sum = 0 loop ke andr isliye likha h becz if the value of sum doesnt matches the if condition we reset the sum again to 0 becz hme contigous subarray chahhiye 
