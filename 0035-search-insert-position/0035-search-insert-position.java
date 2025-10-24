8b37a9493593417cb8458ee4750b3c3fcfaf6286
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-10-24 20:53:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,14 +3,15 @@ class Solution {
         int n = nums.length;
         int st = 0;
         int end = n-1;
+        int ans = -1;
         while(st <= end){
-            int mid = st + (end-st)/2;
+            int mid = st + (end - st) / 2;
             if(nums[mid] == target) return mid;
-            if(target > nums[mid]){
-                st = mid+1;
+            else if(target < nums[mid]){
+                end = mid - 1;
             }
             else{
-                end = mid-1;
+                st = mid + 1;
             }
         }
         return st;
