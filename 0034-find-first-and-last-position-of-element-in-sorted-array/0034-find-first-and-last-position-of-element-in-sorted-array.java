3868d5c128199e3fb9ce3720fad8e30bdef12ba7
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-10-29 10:48:54 +0530

    Time: 0 ms (100%), Space: 45.5 MB (98.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -7,12 +7,13 @@ class Solution {
         int st = 0, end = n - 1;
         while (st <= end) {
             int mid = st + (end - st) / 2;
-            if (nums[mid] == target) {
-                ans[0] = mid;   // store first index
-                end = mid - 1;  // keep searching left side
+            if (nums[mid] > target) {
+                end = mid - 1;   // store first index
+                  // keep searching left side
             } else if (nums[mid] < target) {
                 st = mid + 1;
             } else {
+                ans[0] = mid;
                 end = mid - 1;
             }
         }
