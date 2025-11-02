3fff85ed6b5348c16d9d6d1f7d58a87403c3382f
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-11-02 18:37:25 +0530

    Time: 1 ms (85.99%), Space: 42.6 MB (7.11%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -5,11 +5,16 @@ class Solution {
         int end = x;
         while(st <= end){
             int mid = st + (end - st) / 2;
-            if(mid == x/mid) return mid;
-            else if(mid > x/mid) end = mid - 1;
-            else st = mid + 1;
+            if(mid == x/mid){
+                return mid;
+            }
+            else if(mid < x/mid){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
         }
         return end;
     }
-    
 }
