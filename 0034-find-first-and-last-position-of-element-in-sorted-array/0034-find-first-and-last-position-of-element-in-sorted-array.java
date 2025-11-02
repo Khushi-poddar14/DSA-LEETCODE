ff0c4fbcbc570de35e56581803b4b51334b2be20
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-11-02 17:24:12 +0530

    Time: 0 ms (100%), Space: 48.2 MB (9.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int[] ans = {-1, -1};
+        int st = 0;
+        int end = n-1;
+        int[] arr = {-1,-1};
 
-        // First occurrence
-        int st = 0, end = n - 1;
-        while (st <= end) {
+        while(st <= end){
             int mid = st + (end - st) / 2;
-            if (nums[mid] > target) {
-                end = mid - 1;   // store first index
-                  // keep searching left side
-            } else if (nums[mid] < target) {
+            if(nums[mid] == target){
+                arr[0] = mid;
+                end = mid - 1;
+            }
+            else if(target > nums[mid]){
                 st = mid + 1;
-            } else {
-                ans[0] = mid;
+            }
+            else{
                 end = mid - 1;
             }
         }
-
-        // Last occurrence
         st = 0;
-        end = n - 1;
-        while (st <= end) {
+        end = n-1;
+        while(st <= end){
             int mid = st + (end - st) / 2;
-            if (nums[mid] == target) {
-                ans[1] = mid;   // store last index
-                st = mid + 1;   // keep searching right side
-            } else if (nums[mid] < target) {
+            if(nums[mid] == target){
+                arr[1] = mid;
                 st = mid + 1;
-            } else {
+            }
+            else if(target > nums[mid]){
+                st = mid + 1;
+            }
+            else{
                 end = mid - 1;
             }
         }
-
-        return ans;
+        return arr;
     }
 }
