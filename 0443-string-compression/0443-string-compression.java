2c66bac43c5315f5e3f5360adc39d9be38ded856
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-01-02 12:55:25 +0530

    Time: 1 ms (99.78%), Space: 45.5 MB (45.31%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,22 +1,18 @@
 class Solution {
     public int compress(char[] chars) {
         int n = chars.length;
-        int index = 0;
         int i = 0;
-        while(i < n){
+        int index = 0;
+        while(i<n){
             char curr = chars[i];
             int count = 0;
-
-            //find count of duplicates
-            while(i<n && curr == chars[i]){
+            while(i < n && curr == chars[i]){
                 count++;
                 i++;
             }
-
-            //Now Assign
-            chars[index] = curr;
+            chars[index]=curr;
             index++;
-            if(count > 1){
+            if(count>1){
                 String cnt = Integer.toString(count);
                 for(char ch : cnt.toCharArray()){
                     chars[index] = ch;
