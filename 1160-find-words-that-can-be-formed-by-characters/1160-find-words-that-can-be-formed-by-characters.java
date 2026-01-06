5d33403c0834de4064cb7e13a8bded1f1646a010
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-01-06 19:13:32 +0530

    Time: 5 ms (87.39%), Space: 47.6 MB (53.12%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,23 +1,75 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int[] charCount = new int[26];
+        // int[] charCount = new int[26];
+
+        // for(char ch : chars.toCharArray()){
+        //     charCount[ch - 'a']++;
+        // }
+
+        // int result = 0;  // to store length of good string
+
+        // for(String word : words){
+        //     int[] wordCount = new int[26];  // loop ke andr array isliye bnaya h becz har array ki apni character frequency hogi
+
+        //     for(char sh : word.toCharArray()){
+        //         wordCount[sh - 'a']++;
+        //     }
+
+        //     boolean ok = true;
+
+        //     for(int i = 0; i < 26; i++){
+        //         if(wordCount[i] > charCount[i]){
+        //             ok = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(ok == true){
+        //         result += word.length();
+        //     }
+        // }
+        // return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int[] charfreq = new int[26];
         for(char ch : chars.toCharArray()){
-            charCount[ch - 'a']++;
+            charfreq[ch - 'a']++;
         }
         int result = 0;
         for(String word : words){
-            int[] wordCount = new int[26];
-            for(char sh : word.toCharArray()){
-                wordCount[sh - 'a']++;
-            }
+            int[] wordfreq = new int[26];
             boolean ok = true;
+            for(char ch : word.toCharArray())
+            {
+                wordfreq[ch - 'a']++;
+            }
             for(int i = 0; i < 26; i++){
-                if(wordCount[i] > charCount[i]){
+                if(wordfreq[i] > charfreq[i]){
                     ok = false;
                     break;
                 }
             }
-            if(ok == true){
+            if(ok){
                 result += word.length();
             }
         }
