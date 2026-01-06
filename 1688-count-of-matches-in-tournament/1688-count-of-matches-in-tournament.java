f0e7385ff3ae4ca1f3593ebc56aef78a3740b40b
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-01-06 23:01:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (14.2%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,5 +1,25 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
+        // Har match → 1 team eliminate
+        // Final me 1 winner bachta hai
+        // Total matches = n-1;
+
+        // Code I
+        // return n-1;
+
+
+        // Code II
+        int matches = 0;
+        while(n > 1){
+            if(n % 2 == 0){
+                matches += n/2;
+                n = n/2;    // itni no. of teams will go to play in next round
+            }
+            else{
+                matches += (n-1) / 2;
+                n = ((n-1)/2)+1;    // itni no. of teams will go to play in next round
+            }
+        }
+        return matches;
     }
 }
