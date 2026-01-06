367ff10fe4a9f4b2a067b16b174bd4de0aad0970
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-01-06 14:50:27 +0530

    Time: 3 ms (40.5%), Space: 43.3 MB (21.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,14 +1,32 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         // code I
-        int s_sum = 0;
-        int t_sum = 0;
-        for(int i = 0; i < s.length(); i++){
-            s_sum += (int)s.charAt(i);
+        // int s_sum = 0;
+        // int t_sum = 0;
+        // for(int i = 0; i < s.length(); i++){
+        //     s_sum += (int)s.charAt(i);
+        // }
+        // for(int j = 0; j < t.length(); j++){
+        //     t_sum += (int)t.charAt(j);
+        // }
+        // return (char)(t_sum - s_sum);
+
+
+        // code II
+        int[] freq = new int[26];
+        for(int i = 0; i < t.length(); i++){
+            int index = t.charAt(i) - 'a';
+            freq[index]++;
         }
-        for(int j = 0; j < t.length(); j++){
-            t_sum += (int)t.charAt(j);
+        for(int j = 0; j < s.length(); j++){
+            int idx = s.charAt(j) - 'a';
+            freq[idx]--;
         }
-        return (char)(t_sum - s_sum);
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0){
+                return (char)(i + 'a');
+            }
+        }
+        return ' ';
     }
 }
